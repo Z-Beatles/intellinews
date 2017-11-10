761222c573f952459be63a5d607bc68dd0cf5d09
Wayne Chu <waynechu@waynechu.cn>
2017-11-10 15:48:23 +0800

    修改获取最新文章的方法，通过排序获取

--- src/main/java/com/fintech/intellinews/dao/ArticleDao.java
+++ src/main/java/com/fintech/intellinews/dao/ArticleDao.java
@@ -2,11 +2,16 @@ package com.fintech.intellinews.dao;
 
 import com.fintech.intellinews.base.BaseDao;
 import com.fintech.intellinews.entity.ArticleEntity;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ArticleDao extends BaseDao<ArticleEntity> {
+    /**
+     * 获取最新文章列表
+     *
+     * @return 文章列表
+     */
+    List<ArticleEntity> listLatestArticles();
 }
