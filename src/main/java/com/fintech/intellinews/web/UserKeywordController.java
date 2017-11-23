67b160d3a541917bbcb1edcf4ea5d73579cfb900
806649821@qq.com <wanghao13145I0>
2017-11-23 13:10:30 +0800

    用户偏好资源模块v0.1

--- src/main/java/com/fintech/intellinews/web/UserKeywordController.java
+++ src/main/java/com/fintech/intellinews/web/UserKeywordController.java
@@ -1,11 +1,11 @@
 package com.fintech.intellinews.web;
 
 import com.fintech.intellinews.Result;
-import com.fintech.intellinews.entity.UserKeywordEntity;
 import com.fintech.intellinews.service.UserKeywordService;
 import com.fintech.intellinews.service.UserService;
 import com.fintech.intellinews.util.ResultUtil;
 import com.fintech.intellinews.vo.UserKeywordVO;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +18,7 @@ import java.util.List;
  * create 2017-11-23 11:06
  **/
 @RestController
+@Api(tags = "用户偏好关键字资源")
 @RequestMapping("/v1/user/keywords")
 public class UserKeywordController {
 
@@ -29,7 +30,7 @@ public class UserKeywordController {
     @ResponseBody
     @ApiOperation(value = "获取用户偏好关键字", produces = "application/json")
     public Result<List<UserKeywordVO>> getUserHobby(
-            @ApiParam(value = "userId",name="用户id",required = true)
+            @ApiParam(name="userId",value = "用户id",required = true)
             @RequestParam Long userId){
         userService.checkCurrentUser(userId);
         return ResultUtil.success(userKeywordService.getUserKeyWords(userId));
@@ -39,12 +40,24 @@ public class UserKeywordController {
     @ResponseBody
     @ApiOperation(value = "添加用户偏好关键字", produces = "application/json")
     public Result<UserKeywordVO> addUserHobby(
-            @ApiParam(value = "userId",name="用户id",required = true)
+            @ApiParam(name="userId",value = "用户id",required = true)
             @RequestParam Long userId,
-            @ApiParam(value = "keyword",name="用户偏好关键字",required = true)
+            @ApiParam(name="keyword",value = "用户偏好关键字",required = true)
             @RequestParam String keyword){
         userService.checkCurrentUser(userId);
         return ResultUtil.success(userKeywordService.addUserKeyword(userId,keyword));
+    }
+
+    @PutMapping
+    @ResponseBody
+    @ApiOperation(value = "更新用户偏好关键字", produces = "application/json")
+    public Result<UserKeywordVO> updateUserHobby(
+            @ApiParam(name="userId",value = "用户id",required = true)
+            @RequestParam Long userId,
+            @ApiParam(name="keyword",value = "用户偏好关键字",required = true)
+            @RequestParam String keyword){
+        userService.checkCurrentUser(userId);
+        return ResultUtil.success(userKeywordService.updateHobbyAttention(userId,keyword));
     }
 
     @Autowired
