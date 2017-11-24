f2863bec9db44e847def308f5f5b848038defef2
Wayne Chu <waynechu@waynechu.cn>
2017-11-24 10:44:30 +0800

    完成获取热搜关键字接口

--- src/main/java/com/fintech/intellinews/dao/KeywordDao.java
+++ src/main/java/com/fintech/intellinews/dao/KeywordDao.java
@@ -4,6 +4,22 @@ import com.fintech.intellinews.base.BaseDao;
 import com.fintech.intellinews.entity.KeywordEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KeywordDao extends BaseDao<KeywordEntity> {
+    /**
+     * 获取热门关键字
+     *
+     * @return 关键字列表
+     */
+    List<KeywordEntity> listHotKeywords();
+
+    /**
+     * 更新关键字热度
+     *
+     * @param keyword 关键字
+     * @return 影响的行数
+     */
+    int updateKeywordCount(String keyword);
 }
--- src/main/java/com/fintech/intellinews/entity/KeywordEntity.java
+++ src/main/java/com/fintech/intellinews/entity/KeywordEntity.java
@@ -5,6 +5,26 @@ import com.fintech.intellinews.base.BaseEntity;
 public class KeywordEntity extends BaseEntity {
     private Long id;
 
+    private String name;
+
+    private int count;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
     public Long getId() {
         return id;
     }
