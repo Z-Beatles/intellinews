23f1ae79e44ed8f9b51316bb398578b35489972c
806649821@qq.com <wanghao13145I0>
2017-12-07 15:37:33 +0800

    使用建造者模式构建DruidDataSource对象，残留bug：多数据源监控、sql监控、shiro安全框架登录会话遗失

--- src/main/java/com/fintech/intellinews/config/DynamicDataSourceConfig.java
+++ src/main/java/com/fintech/intellinews/config/DynamicDataSourceConfig.java
@@ -1,10 +1,10 @@
 package com.fintech.intellinews.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import com.fintech.intellinews.base.dynamic.DynamicDataSource;
+import com.fintech.intellinews.datasource.DruidDataSourceFactory;
+import com.fintech.intellinews.datasource.dynamic.DynamicDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +15,6 @@ import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.Arrays;
 
 /**
@@ -25,56 +24,31 @@ import java.util.Arrays;
 @Configuration
 public class DynamicDataSourceConfig {
 
-    @Bean("abstractDataSource")
-    public static DruidDataSource abstractDataSource() {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setInitialSize(2);
-        dataSource.setMaxWait(60000);
-        dataSource.setMaxActive(20);
-        dataSource.setTimeBetweenConnectErrorMillis(6000);
-        dataSource.setMinEvictableIdleTimeMillis(300000);
-        dataSource.setValidationQuery("SELECT 'x'");
-        dataSource.setTestOnBorrow(true);
-        dataSource.setTestOnReturn(false);
-        dataSource.setPoolPreparedStatements(true);
-        dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
-        dataSource.setPoolPreparedStatements(true);
-        try {
-            dataSource.setFilters("mergeStat");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return dataSource;
-    }
-
     @Bean("master")
-    public DruidDataSource master(@Qualifier("abstractDataSource") DruidDataSource druidDataSource) {
-        DruidDataSource master = new DruidDataSource();
-        BeanUtils.copyProperties(master, druidDataSource);
-        master.setUrl("jdbc:mysql://119.31.210.76:3316/intellinews?characterEncoding=utf8&useSSL=false");
-        master.setUsername("root");
-        master.setPassword("WRy93Ee4sGI");
-        return master;
+    public DruidDataSource master() {
+        return new DruidDataSourceFactory.DruidDataSourceBuilder()
+                .setUsername("root")
+                .setPassword("AE6eiO3r.E1")
+                .setUrl("jdbc:mysql://119.31.210.76:3316/intellinews?characterEncoding=utf8&useSSL=false")
+                .build();
     }
 
     @Bean("slave1")
-    public DruidDataSource slave1(@Qualifier("abstractDataSource") DruidDataSource druidDataSource) {
-        DruidDataSource slave1 = new DruidDataSource();
-        BeanUtils.copyProperties(slave1, druidDataSource);
-        slave1.setUrl("jdbc:mysql://119.31.210.76:3316/intellinews?characterEncoding=utf8&useSSL=false");
-        slave1.setUsername("root");
-        slave1.setPassword("WRy93Ee4sGI");
-        return slave1;
+    public DruidDataSource slave1() {
+        return new DruidDataSourceFactory.DruidDataSourceBuilder()
+                .setUsername("root")
+                .setPassword("AE6eiO3r.E1")
+                .setUrl("jdbc:mysql://119.31.210.76:3317/intellinews?characterEncoding=utf8&useSSL=false")
+                .build();
     }
 
     @Bean("slave2")
-    public DruidDataSource slave2(@Qualifier("abstractDataSource") DruidDataSource druidDataSource) {
-        DruidDataSource slave2 = new DruidDataSource();
-        BeanUtils.copyProperties(slave2, druidDataSource);
-        slave2.setUrl("jdbc:mysql://119.31.210.76:3316/intellinews?characterEncoding=utf8&useSSL=false");
-        slave2.setUsername("root");
-        slave2.setPassword("WRy93Ee4sGI");
-        return slave2;
+    public DruidDataSource slave2() {
+        return new DruidDataSourceFactory.DruidDataSourceBuilder()
+                .setUsername("root")
+                .setPassword("AE6eiO3r.E1")
+                .setUrl("jdbc:mysql://119.31.210.76:3318/intellinews?characterEncoding=utf8&useSSL=false")
+                .build();
     }
 
     @Bean("dynamicDataSource")
@@ -84,7 +58,7 @@ public class DynamicDataSourceConfig {
             @Qualifier("slave2") DataSource slave2) {
         DynamicDataSource dynamic = new DynamicDataSource();
         dynamic.setMaster(master);
-        dynamic.setSlaves(Arrays.asList(slave1,slave2));
+        dynamic.setSlaves(Arrays.asList(slave1, slave2));
         return dynamic;
     }
 
@@ -96,7 +70,7 @@ public class DynamicDataSourceConfig {
     }
 
     @Bean("transactionManager")
-    public DataSourceTransactionManager transactionManager(@Qualifier("abstractDataSource") DataSource dataSource){
+    public DataSourceTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
         DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
         transactionManager.setDataSource(dataSource);
         return transactionManager;
